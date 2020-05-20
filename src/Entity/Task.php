e87fdb57b77004690a1cea50bf1ae1b1b7288b19
Gumil <szymon489984@gmail.com>
2020-05-20 15:03:31 +0200

    poprawka encji Task

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -28,7 +28,7 @@ class Task
     private $created;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      */
     private $solution;
 
